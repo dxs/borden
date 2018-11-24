eb689e8d3d2e900207ce4eccb5c641004e1b55d8
dxs <glaglasven@live.com>
2018-11-24 18:33:57 +0100

    First Update

---
+++ zyro/2.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Gallery</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!-- Facebook Open Graph -->
+	<meta property="og:title" content="Gallery" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20181122020448" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20181122020449" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1543005071" rel="stylesheet" type="text/css" />
+	<link href="css/2.css?ts=1543005071" rel="stylesheet" type="text/css" />
+	<script src="js/jquery.browser.min.js" type="text/javascript"></script>
+	<link href="js/photoswipe/photoswipe.css" rel="stylesheet" type="text/css" />
+	<link href="js/photoswipe/default-skin/default-skin.css" rel="stylesheet" type="text/css" />
+	<script src="js/photoswipe/photoswipe.min.js" type="text/javascript"></script>
+	<script src="js/photoswipe/photoswipe-ui-default.min.js" type="text/javascript"></script>
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance22" class="wb_element wb_text_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle">Sven</h4></div><div id="wb_element_instance23" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">Home</a></li><li class="active"><a href="Gallery/" target="_self">Gallery</a></li><li><a href="Contacts/" target="_self">Contacts</a></li></ul><div class="clearfix"></div></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance24" class="wb_element wb_element_picture" title=""><a href="http://facebook.com" target="1"><img alt="gallery/1461561521_facebook_online_social_media" src="gallery_gen/2094e6259696f9ac364a8fd6c10b8918.png"></a></div><div id="wb_element_instance25" class="wb_element wb_element_picture" title=""><a href="http://instagram.com"><img alt="gallery/1461561535_instagram_online_social_media" src="gallery_gen/971c833147bf81ae8bb9e3868bb7fc38.png"></a></div><div id="wb_element_instance26" class="wb_element wb_element_picture" title=""><a href="http://pinterest.com" target="1"><img alt="gallery/1461561581_pinterest_online_social_media" src="gallery_gen/fead0a2864a983549335612d0d471c73.png"></a></div><div id="wb_element_instance28" class="wb_element wb_gallery"><script type="text/javascript" src="js/WB_Gallery.class.js?v=20181122020448"></script><script type="text/javascript">
+				$(function() {
+					new WB_Gallery({"id":"wb_element_instance28","type":"slideshow","interval":10,"speed":400,"fullWidth":false,"imageCover":false,"width":910,"height":380,"border":{"color":"#FFFFFF","style":"solid","weight":5,"radius":null,"css":{"border":"5px solid #FFFFFF"}},"padding":5,"thumbWidth":64,"thumbHeight":64,"thumbAlign":"left","images":[{"thumb":"gallery_gen\/e31bc629df358f7f0cf3f7ae3c647077_64x64.jpeg","image":"gallery_gen\/4248f3adbf4027a3d42e3bfa9616486b.jpeg","width":640,"height":426},{"thumb":"gallery_gen\/3f86b9e71c587f601ec6a1666beb1188_64x64.jpeg","image":"gallery_gen\/2f5af76ee1e069c08e191bd92294121b.jpeg","width":640,"height":425},{"thumb":"gallery_gen\/f911881d5e6ba1a74566519b26fae63e_64x64.jpeg","image":"gallery_gen\/11bbdfa6349a3e0dfb69df75261253ba.jpeg","width":640,"height":371},{"thumb":"gallery_gen\/2b7549c5e7e5bb853b28530f7addf5fc_64x64.jpg","image":"gallery_gen\/340b2d1ae6c4d98aee237a7d1d0f95ae.jpg","width":640,"height":427},{"thumb":"gallery_gen\/33334e13ac2ef989c06a2f271e52eb48_64x64.jpg","image":"gallery_gen\/9bbae255eecb190438397988f9f3c460.jpg","width":848,"height":1280},{"thumb":"gallery_gen\/e2d4a8182a165171de2927454352dc86_64x64.jpg","image":"gallery_gen\/5abcfea5858de48bc25c348174c6bd31.jpg","width":640,"height":426},{"thumb":"gallery_gen\/47f6214232e8d7e19913f60a2448c2ef_64x64.jpeg","image":"gallery_gen\/bc53194333e0c0e6751f130e9d7481d4.jpeg","width":640,"height":426},{"thumb":"gallery_gen\/2a65ebded92f86fc867a103de3cecfc5_64x64.jpeg","image":"gallery_gen\/1af73e3bfa28ca09fcf41c4629639db1.jpeg","width":1920,"height":1174},{"thumb":"gallery_gen\/7b8a20f02b3a2f00f815b93a73f3a3a7_64x64.jpg","image":"gallery_gen\/f785be03c3d4b8fc38ca781b876f9f21.jpg","width":640,"height":425},{"thumb":"gallery_gen\/389c8897f13efdf9336d0d6f9212f0f6_64x64.jpeg","image":"gallery_gen\/206aaa57290d9b4f55edf618845c01f2.jpeg","width":640,"height":426}]});
+				});
+			</script></div><div id="wb_element_instance29" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance30" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest...</p>
+<p class="wb-stl-normal"> </p>
+<p class="wb-stl-normal">Information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this...</p></div><div id="wb_element_instance31" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(2);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance31");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance31").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 124px;"><div id="wb_element_instance27" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2018 <a href="http://borden.ch">borden.ch</a></p></div><div id="wb_element_instance32" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>
+<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="pswp__bg" style="opacity: 0.7;"></div>
+	<div class="pswp__scroll-wrap">
+		<div class="pswp__container">
+			<div class="pswp__item"></div>
+			<div class="pswp__item"></div>
+			<div class="pswp__item"></div>
+		</div>
+		<div class="pswp__ui pswp__ui--hidden">
+			<div class="pswp__top-bar">
+				<div class="pswp__counter"></div>
+				<button class="pswp__button pswp__button--close" title="Close"></button>
+				<button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+				<div class="pswp__preloader">
+					<div class="pswp__preloader__icn">
+						<div class="pswp__preloader__cut">
+							<div class="pswp__preloader__donut"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+				<div class="pswp__share-tooltip"></div> 
+			</div>
+			<button class="pswp__button pswp__button--arrow--left" title="Previous"></button>
+			<button class="pswp__button pswp__button--arrow--right" title="Next"></button>
+			<div class="pswp__caption"><div class="pswp__caption__center"></div></div>
+		</div>
+	</div>
+</div>
+{{hr_out}}</body>
+</html>
